83b3653ef8666cca56b2ac6c8441a94210645f90
Acer <konskiidima@gmail.com>
2025-12-29 16:37:43 +0100

    implement py-person-class

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people):
+    for data in people:
+        Person(data["name"], data["age"])
+
+    for data in people:
+        person = Person.people[data["name"]]
+
+        if "wife" in data and data["wife"] is not None:
+            person.wife = Person.people[data["wife"]]
+
+        if "husband" in data and data["husband"] is not None:
+            person.husband = Person.people[data["husband"]]
+
+    return list(Person.people.values())
